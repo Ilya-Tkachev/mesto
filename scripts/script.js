88dcf0c3e55460c8ae4a09a7124839d553b39951
Ilya Tkachev <itkachev@vertafore.com>
2020-10-31 18:59:18 +0300

    done

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+const profileFieldName = document.querySelector('.profile__heading');
+const profileFieldInfo = document.querySelector('.profile__description');
+const profileEditButton = document.querySelector('.button_type_eddit');
+
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.button_type_close');
+const popupFieldName = document.querySelector('#popupFieldName');
+const popupFieldInfo = document.querySelector('#popupFieldInfo');
+const popupSaveButton = document.querySelector('.button_type_save');
+
+function openPopup() {
+    popupFieldName.value = profileFieldName.textContent;
+    popupFieldInfo.value = profileFieldInfo.textContent;
+    popup.classList.remove('popup_state_closed');
+}
+
+function closePopup() {
+    popup.classList.add('popup_state_closed');
+}
+
+function isPopupOpened() {
+    return !popup.classList.contains('popup_state_closed');
+}
+
+function savePopupChanges() {
+    profileFieldName.textContent = popupFieldName.value;
+    profileFieldInfo.textContent = popupFieldInfo.value;
+    closePopup();
+}
+
+profileEditButton.addEventListener('click', openPopup);
+
+popupCloseButton.addEventListener('click', closePopup);
+
+popupSaveButton.addEventListener('click', savePopupChanges);
+
+document.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter' && isPopupOpened()) {
+        savePopupChanges();
+    }    
+  });
